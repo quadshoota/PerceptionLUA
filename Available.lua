f3d8d751b954eced35ac78c250c797757e410663
quadshoota <95818124+quadshoota@users.noreply.github.com>
2025-10-20 20:06:24 +0200

    Add files via upload

---
+++ Available.lua
@@ -0,0 +1,9 @@
+local Available = {
+"arrowdown", "arrowleft", "arrowright", "arrowup",
+"browser", "browsers", "calender", "cloudfile",
+"codeblock", "delete", "download", "downloadcloud",
+"gear", "upload", "uploadtocloud", "archive",
+"checkmark", "copy", "refresh", "user", "date", "version", "Egg", "Dog", "Tree", "Money", "weight", "coin", "crown", "Doki", "questionmark", "inferno", "pcnpng", "rocket", "star", "trophy", "unicorn", "wizard", "yinyang"
+}
+ 
+return Available
